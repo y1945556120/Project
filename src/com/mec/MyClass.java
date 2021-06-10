bf99010d70c92185efc909c918a5abf3e08b6c69
yhj <1945556120@qq.com>
2021-06-10 20:01:20 +0800

    four commit

--- src/com/mec/MyClass.java
+++ src/com/mec/MyClass.java
@@ -7,6 +7,7 @@ public class MyClass {
         System.out.println("hot-fix分支修改");
         System.out.println("合并冲突");
         System.out.println("hot-fix合并冲突");
+        System.out.println("nihao");
     }
 
 }
