fc3270ed1ad91d2adb6f68e59e5680c5d5c8af09
yhj <1945556120@qq.com>
2021-06-10 17:27:38 +0800

    hot-fix commit1

--- src/com/mec/MyClass.java
+++ src/com/mec/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("myclass");
         System.out.println("这是第二次提交");
+        System.out.println("hot-fix分支修改");
     }
 
 }
